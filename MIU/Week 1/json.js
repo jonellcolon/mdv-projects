fa9c174ea7c35c548891a2ffd8fee418feab9c87
Jonell Colon <jonellcolon@fullsail.edu>
2012-04-30 22:19:44 -0400

    Added VFW Final project to MIU folder

---
+++ MIU/Week 1/json.js
@@ -0,0 +1,47 @@
+// JSON Object
+
+var json = {
+	"part1": {
+		"group": ["Group:", "Engine"],
+		"fullname": ["Full Name:", "Jonell Colon"],
+		"phone": ["Phone:", "407-341-5269"],
+		"email": ["E-Mail:", "jonellcolon@aol.com"],
+		"cpart": ["Car Part:", "Rotor"],
+		"hmany": ["How Many:", "2"],
+		"ctype": ["Car Type:", "Honda"],
+		"cmodel": ["Car Model:", "Civic"],
+		"ycar": ["Car Year:", "2001"],
+		"mode": ["Value:", "New"],
+		"special": ["Special Request:", "Leave On"]
+	
+	},
+	"part2": {
+		"group": ["Group:", "Cabin"],
+		"fullname": ["Full Name:", "Rose Ayala"],
+		"phone": ["Phone:", "407-437-7526"],
+		"email": ["E-Mail:", "roseayala.colon@gmail.com"],
+		"cpart": ["Car Part:", "Seats"],
+		"hmany": ["How Many:", "4"],
+		"ctype": ["Car Type:", "Toyota"],
+		"cmodel": ["Car Model:", "Sienna"],
+		"ycar": ["Car Year:", "2003"],
+		"mode": ["Value:", "Used"],
+		"special": ["Special Request:", "Do not lend."]
+	},
+	"part3": {
+		"group": ["Group:", "Wheels"],
+		"fullname": ["Full Name:", "Eliana Colon"],
+		"phone": ["Phone:", "407-249-0896"],
+		"email": ["E-Mail:", "eli@newbirthorlando.org"],
+		"cpart": ["Car Part:", "Hub Caps"],
+		"hmany": ["How Many:", "2"],
+		"ctype": ["Car Type:", "Ford"],
+		"cmodel": ["Car Model:", "F-150"],
+		"ycar": ["Car Year:", "1998"],
+		"mode": ["Value:", "New"],
+		"special": ["Special Request:", "Save old."]
+		
+	}
+		
+}
+	
